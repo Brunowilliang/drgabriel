c8a4c50fe64d75c103bed59dd7b9f479eb9070f7
Bruno Garcia <brunowilliang@icloud.com>
2025-04-09 15:07:34 +0100

    fix: update cursoPresencial link to new payment URL

--- src/utils/links.tsx
+++ src/utils/links.tsx
@@ -3,7 +3,7 @@ export const links = {
 	cursoPlanoOnline:
 		'https://pay.kirvano.com/9bf16f32-653d-4254-b9c3-38723c9f0ff1',
 	cursoPresencial:
-		'https://api.whatsapp.com/send/?phone=5518997240779&text&type=phone_number&app_absent=0',
+		'https://pay.kirvano.com/3e3d5efc-43de-4066-b655-190a601ebd72',
 	instagram: 'https://www.instagram.com/institutogr.oficial/',
 	facebook: 'https://www.facebook.com/profile.php?id=61558729928292',
 } as const
